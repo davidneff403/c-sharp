0af7cd0344c8566231169e5bd6a1f77f091a4442
davidneff403 <149708274+davidneff403@users.noreply.github.com>
2026-01-22 10:45:05 -0500

    Add files via upload

--- tutorials/numbers.cs
+++ tutorials/numbers.cs
@@ -1,1 +1,116 @@
-//something scientific
+// Explore integer math
+// WorkWithIntegers();
+// OrderPrecedence();
+// IntegerPrecisionAndLimits();
+// WorkWithDoubles();
+// DecimalType();
+
+using System.Formats.Asn1;
+
+FinalChallenge();
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+
+    // addition
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+// Explore order of operations
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+}
+
+void IntegerPrecisionAndLimits()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine($"quotient: {d}");
+    Console.WriteLine($"remainder: {e}");
+
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"The range of integers is {min} to {max}");
+
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow: {what}");
+}
+
+void WorkWithDoubles()
+{
+    double a = 5;
+    double b = 4;
+    double c = 2;
+    double d = (a + b) / c;
+    Console.WriteLine(d);
+
+    double e = 19;
+    double f = 23;
+    double g = 8;
+    double h = (e + f) / g;
+    Console.WriteLine(h);
+
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"The range of double is {min} to {max}");
+
+    double third = 1.0 / 3.0;
+    Console.WriteLine(third);
+}
+
+void DecimalType()
+{
+    decimal min = decimal.MinValue;
+    decimal max = decimal.MaxValue;
+    Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
+
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
+}
+
+void FinalChallenge()
+{
+    double r = 2.50;
+    double areaOfCircle = (r * r) * Math.PI;
+    Console.WriteLine($"The area of a circle with a radius of {r}cm is {areaOfCircle}");
+}
